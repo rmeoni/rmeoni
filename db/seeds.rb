1f39f39493c1c42433b3efc17f058e1a4139f48d
rmeoni <rodrigo.me13@gmail.com>
2016-07-11 16:16:09 -0600

    Saving before moving the db to psql

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+	user1= User.create(first_name: 'Rodrigo', last_name: 'meoño', email: 'rodrigo.me13@gmail.com', password:'fUngus14.15', role: 'admin')
+
+	p1= Project.create(image: '/images/blancnoir/canoe.jpg', name: 'blanc et noir', description: 'A collection in black and white')
+		ph1 = Photograph.create(:name 'searching', :image '/images/blancnoir/searching.jpg', :iso '100', :exposure '1/100', :project_id = p1.id)
