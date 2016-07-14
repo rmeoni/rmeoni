4318c3082db5f85b21c0242f4d10ca7c52774d7f
rmeoni <rodrigo.me13@gmail.com>
2016-07-14 10:02:59 -0600

    Trying to fix the mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,15 @@ config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
 
-
+config.action_mailer.smtp_settings = {
+address: 'smtp.gmail.com',
+port: 587,
+domain: ENV['GMAIL_DOMAIN'],
+authentication: 'plain',
+enable_starttls_auto: true,
+user_name: ENV['GMAIL_USERNAME'],
+password: ENV['GMAIL_PASSWORD']
+}
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
