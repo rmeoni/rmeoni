afe0c5a7e1dc6ca675be7977cca8920bcac13fbd
rmeoni <rodrigo.me13@gmail.com>
2016-08-12 15:30:32 -0500

    fixing route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   post '/projects/:id/new' => 'photographs#create'
   get '/projects/:id' => 'projects#show', as: :project
   patch '/projects/:id' => 'projects#update'
-  get '/hiatus' => 'projects/4'
 
   get '/photographs' => 'photographs#index'
   post '/photographs' => 'photographs#create'
